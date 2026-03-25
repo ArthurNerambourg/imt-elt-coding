e776563f044c92cde26b2522c7bab7df8d9341b4
SmartinIMT <sacha.martin67@gmail.com>
2026-03-25 14:14:33 +0100

    test_order_line_items

---
+++ tests/test_database.py
@@ -0,0 +1,24 @@
+import pytest
+import pandas as pd
+
+from unittest.mock import patch
+
+from src.database import (
+    get_engine,
+    test_connection,
+    execute_sql
+    )
+@
+Class TestDatabase : 
+
+def test_get_engine():
+        result = get_engine()
+        assert
+
+def test_test_connection():
+        result = test_connection()
+        assert
+
+def test_execute_sql() : 
+        result=test_execute_sql()
+        assert 
